8756d395511c3bd410ad22c8684169fafdfa67bb
kryniax <kryniax@gmail.com>
2022-09-03 23:29:22 +0200

    email update

--- src/inputs/Email.tsx
+++ src/inputs/Email.tsx
@@ -10,7 +10,10 @@ const Email = (props: Props) => {
         <input 
             type="email"
             {...props.register("email", {
-                required: "Email is required"
+                required: "Email is required",
+                pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                }
             })}
             onKeyUp={(e) => {
                 const emailValue = (e.target as HTMLInputElement).value;
